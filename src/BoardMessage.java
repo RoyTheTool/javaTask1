a430dfa8246e5b12a3050cf7db610e04d98d6ce4
roythetool <roymaman222@gmail.com>
2025-11-18 14:37:00 +0200

    boardMessage class finish

--- src/BoardMessage.java
+++ src/BoardMessage.java
@@ -1,22 +1,57 @@
-import java.util.ArrayList;
+import java.util.Date;
 
 public class BoardMessage extends Message {
 //	fields
-	private PriorityType priority_type;
+	private PriorityType priorityType;
 
-	private
-
-	@Override public boolean find(ArrayList<String> messages) {
-
-		return false;
+//	gets and sets
+	public PriorityType getPriorityType() {
+		return priorityType;
 	}
 
-	@Override
-	public String generatePreview() {
+	public void setPriority(PriorityType priorityType) {
+		if (priorityType == null) {
+			throw new IllegalArgumentException("Priority cannot be null");
+		}
+		this.priorityType = priorityType;
+	}
 
-		return null;
+//	constructors
+	public BoardMessage(String sender, String content, String subject, Date sendDate, PriorityType priorityType) {
+		super(sender, content, subject, sendDate);
+		setPriority(priorityType);
+	}
+
+	public BoardMessage(String sender, String content, String subject) {
+		super(sender, content, subject);
+		this.sendDate = new Date();
+		this.priorityType = priorityType.REGULAR;
 	}
 
 //	methods
+	@Override
+	public String toString() {
+		return super.toString() + ", Priority: " + priorityType;
+	}
 
+	public boolean isUrgent() {
+		return this.priorityType == PriorityType.URGENT;
+	}
+
+	public String generatePreview() {
+
+		String shortContent = "";
+		if (content.length() > 15) {
+			int counter = 0;
+			for (char c : content.toCharArray()) {
+				shortContent = shortContent + c;
+				counter++;
+				if (counter == 15)
+					break;
+			}
+		} else
+			shortContent = content;
+
+		return "[Board]" + this.sender + ": " + shortContent;
+	}
 }
